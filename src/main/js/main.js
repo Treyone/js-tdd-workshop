3b8fa9b3e9affb974026ececc975919f31dc7e25
Remi Goyard <rgoyard@gmail.com>
2014-03-01 19:55:13 +0100

    REmoving unused files Adding some jshint rules in jshint rc Adding Documentation

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,21 +1,6 @@
-define(function(require) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-    var util = require('tasks');
-    var color = "#FF0000";
-    console.log(util);
-    //util.add({id:1, title:"ttt"});
-    if( 0 === util.size() ){
-        console.log("0 == " + util.size());
-        util.add({id:1, title:"ttt"});
-        if( 1 === util.size() ){
-            console.log("1 == " + util.size());
-            color = "#00FF00";
-        }
-    }
-    document.body.style.backgroundColor =color;
+function returnTrue() {
+    'use strict';
+    return true;
+}
 
-
-});
+returnTrue();
